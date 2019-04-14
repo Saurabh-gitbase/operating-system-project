f61fb2ef4fbeec8c9ea4f7e80c4f7565ad556da2
starkpa <49409857+starkpa@users.noreply.github.com>
2019-04-14 22:07:09 +0530

    Update osproject.c

--- osproject.c
+++ osproject.c
@@ -181,50 +181,53 @@ mouse(void *arg)
         
         while (dish -> free_dishes <= 0 || dish -> cats_eating > 0 || dish->cats_waiting > 0) 
         {
-            pthread_cond_wait(&dish->free_cv, &dish->mutex);
+            pthread_cond_wait(&dish -> free_cv, &dish -> mutex);
         }
 
-        // starting to eat - first check everything is fine // //12345667//
+        // starting to eat - first check everything is fine // 
         
-        assert(dish->free_dishes > 0);
-        dish->free_dishes--;
-        assert(dish->cats_eating == 0);
-        assert(dish->mice_eating < N_MICE);
-        dish->mice_eating++;
+        assert(dish -> free_dishes > 0);
+        dish -> free_dishes--;
+        assert(dish -> cats_eating == 0);
+        assert(dish -> mice_eating < N_MICE);
+        dish -> mice_eating++;
 
-        /* ...then find the first available free dish */
-        for (i = 0; i < N_DISHES && dish->status[i] != none_eating; i++) ;
-        my_dish = i;
-        assert(dish->status[my_dish] == none_eating);
-        dish->status[my_dish] = mouse_eating;
-        dump_dish("mouse", pthread_self(), "started", dish, my_dish);
-        pthread_mutex_unlock(&dish->mutex);
+        // then find the first available free dish //
         
-        /* eat - mice need to watch for cats becoming hungry */
+        for (i = 0; i < N_DISHES && dish -> status[i] != none_eating; i++) ;
+        my_dish = i;
+        assert(dish -> status[my_dish] == none_eating);
+        dish -> status[my_dish] = mouse_eating;
+        dump_dish("mouse", pthread_self(), "started", dish, my_dish);
+        pthread_mutex_unlock(&dish -> mutex);
+        
+        // eat - mice need to watch for cats becoming hungry //
+        
         gettimeofday(&tp,NULL);
         ts.tv_sec  = tp.tv_sec;
         ts.tv_nsec = tp.tv_usec * 1000;
         ts.tv_sec += MOUSE_EAT;
-        pthread_mutex_lock(&dish->mutex);
-        pthread_cond_timedwait(&dish->cat_cv, &dish->mutex, &ts);
-        pthread_mutex_unlock(&dish->mutex);
         
-        /* finished eating - check everything is fine and update state */
-        pthread_mutex_lock(&dish->mutex);
-        assert(dish->free_dishes < N_DISHES);
-        dish->free_dishes++;
-        assert(dish->cats_eating == 0);
-        assert(dish->mice_eating > 0);
-        dish->mice_eating--;
-        dish->status[my_dish]=none_eating;
+        pthread_mutex_lock ( &dish -> mutex );
+        pthread_cond_timedwait ( &dish -> cat_cv, &dish -> mutex, &ts);
+        pthread_mutex_unlock ( &dish -> mutex );
+        
+        // finished eating - check everything is fine and update state //
+        pthread_mutex_lock ( &dish -> mutex );
+        assert( dish -> free_dishes < N_DISHES );
+        dish -> free_dishes++;
+        assert ( dish -> cats_eating == 0);
+        assert (dish -> mice_eating > 0);
+        dish -> mice_eating--;
+        dish -> status[my_dish] = none_eating;
 
-        /* finally tell others we're done eating */
-        pthread_cond_broadcast(&dish->free_cv);
-        dump_dish("mouse", pthread_self(), "finished", dish, my_dish);
-        pthread_mutex_unlock(&dish->mutex);
+        // finally tell others we're done eating //
+        pthread_cond_broadcast( &dish -> free_cv);
+        dump_dish ("mouse", pthread_self(), "finished", dish, my_dish);
+        pthread_mutex_unlock ( &dish -> mutex);
         
-        /* sleep some time to give others a chance to grab some food */
-        sleep(rand() % MOUSE_WAIT);
+        // sleep some time to give others a chance to grab some food //
+        sleep( rand() % MOUSE_WAIT);
     }
 
     return NULL;
@@ -240,47 +243,52 @@ main(int argc, char *argv[])
 {
     int i, err;
     dish_t _dish, *dish;
-    pthread_t cats[N_CATS];
-    pthread_t mice[N_MICE];
+    pthread_t cats [N_CATS];
+    pthread_t mice [N_MICE];
 
-    srand(time(NULL));  /* initialize random number generator */
+    srand ( time(NULL) );  // initialize random number generator //
 
     dish = &_dish;
-    memset(dish, 0, sizeof(dish_t));
-    dish->free_dishes = N_DISHES;
-    pthread_mutex_init(&dish->mutex, NULL);
-    pthread_cond_init(&dish->free_cv, NULL);
-    pthread_cond_init(&dish->cat_cv, NULL);
+    memset ( dish, 0, sizeof(dish_t));
+    dish -> free_dishes = N_DISHES;
+    pthread_mutex_init ( &dish -> mutex, NULL);
+    pthread_cond_init ( &dish -> free_cv, NULL);
+    pthread_cond_init ( &dish -> cat_cv, NULL);
     
-    /* create cats */
-    for (i = 0; i < N_CATS; i++) {
-        err = pthread_create(&cats[i], NULL, cat, dish);
-        if (err != 0) {
-            fprintf(stderr, "%s: %s: unable to create cat thread %d: %d\n",
-                    progname, __func__, i, err);
+    // create cats //
+    for (i = 0; i < N_CATS; i++) 
+    {
+        err = pthread_create ( &cats[i], NULL, cat, dish);
+        if (err != 0) 
+        {
+            fprintf(stderr, "%s: %s: unable to create cat thread %d: %d\n",progname, __func__, i, err);
         }
     }
 
-    /* create mice */
-    for (i = 0; i < N_MICE; i++) {
-        err = pthread_create(&mice[i], NULL, mouse, dish);
-        if (err != 0) {
-            fprintf(stderr, "%s: %s: unable to create mouse thread %d: %d\n",
-                    progname, __func__, i, err);
+    // create mice //
+    for (i = 0; i < N_MICE; i++) 
+    {
+        err = pthread_create (&mice[i], NULL, mouse, dish);
+        if (err != 0) 
+        {
+            fprintf(stderr, "%s: %s: unable to create mouse thread %d: %d\n",progname, __func__, i, err);
         }
     }
 
-    /* join all pets */
-    for (i = 0; i < N_CATS; i++) {
-        (void) pthread_join(cats[i], NULL);
-    }
-    for (i = 0; i < N_MICE; i++) {
-        (void) pthread_join(mice[i], NULL);
+    // join cat and mice //
+    for (i = 0; i < N_CATS; i++) 
+    {
+        (void) pthread_join ( cats[i], NULL);
     }
     
-    pthread_mutex_destroy(&dish->mutex);
-    pthread_cond_destroy(&dish->free_cv);
-    pthread_cond_destroy(&dish->cat_cv);
+    for (i = 0; i < N_MICE; i++) 
+    {
+        (void) pthread_join ( mice[i], NULL);
+    }
+    
+    pthread_mutex_destroy ( &dish -> mutex);
+    pthread_cond_destroy ( &dish -> free_cv);
+    pthread_cond_destroy ( &dish -> cat_cv);
     
     return EXIT_SUCCESS;
 }
